a4fd37f53037c6e5c299a45305b214f51de188e9
Diana-PC <carolinabravo946@gmail.com>
2023-06-11 15:03:26 -0500

    Se agrego la clase CalculatorExecute Se agrego al .jar

--- src/main/java/epn/edu/ec/CalculatorExecute.java
+++ src/main/java/epn/edu/ec/CalculatorExecute.java
@@ -1,4 +1,18 @@
 package epn.edu.ec;
 
 public class CalculatorExecute {
+    public static void main(String[] args) {
+        System.out.println("Ejecutable");
+        Calculator c = new Calculator();
+        int suma = c.suma(13, 2);
+        System.out.println("Suma: " + "c.suma(13,2) = " + suma);
+        int resta = c.resta(13, 2);
+        System.out.println("Resta: " + "c.resta(13,2) = " + resta);
+
+        int multiplicacion = c.multiplicacion(13, 2);
+        System.out.println("Multiplicacion: " + "c.multiplicacion(13,2) = " + multiplicacion);
+
+        double division = c.division(13, 2);
+        System.out.println("Division: " + "c.division(13,2) = " + division);
+    }
 }
